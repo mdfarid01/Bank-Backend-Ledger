4251f41e4d80e3cd4526aa759c5fc2a136e62ccf
Md Farid <mdfarid.0118@gmail.com>
2026-02-17 18:59:33 +0530

    Initial clean commit

---
+++ src/controller/auth.controller.js
@@ -0,0 +1,88 @@
+const userModel = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+
+/**
+* - User Registration Controller
+* - Route: POST /api/auth/register
+* - Access: Public
+*/
+async function userRegisterController(req, res) {
+
+     const { email, password,name } = req.body;
+
+     const isExists = await userModel.findOne({
+           email:email 
+          })
+     if (isExists) {
+          return res.status(422).json({
+               success: false,
+               message: 'Email already exists',
+               status: "failed"
+          })
+     }
+
+     const user = await userModel.create({
+          email,
+          password,
+           name
+     })
+
+     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+          expiresIn: '7d'
+     })
+     res.cookie('token', token)
+     res.status(201).json({
+          user: {
+               _id: user._id,
+               email: user.email,
+               name: user.name 
+          },
+          token
+     })
+}
+
+
+/**
+* - User Login Controller
+* - Route: POST /api/auth/login
+* - Access: Public
+*/
+async function userLoginController(req, res) {
+     const { email, password } = req.body;
+
+     const user = await userModel.findOne({ email }).select('+password');
+
+     if(!user) {
+          return res.status(404).json({
+               message: 'User not found',
+               success: false,
+               status: "failed"
+          })
+     }
+     
+    const isValidPassword = await user.comparePassword(password);
+
+    if(!isValidPassword) {
+          return res.status(401).json({
+               message: 'Invalid password',
+          })
+    }
+
+   const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+          expiresIn: '7d'
+     })
+     res.cookie('token', token)
+     res.status(200).json({
+          user: {
+               _id: user._id,
+               email: user.email,
+               name: user.name 
+          },
+          token
+     })       
+}
+
+module.exports = {
+    userRegisterController,
+    userLoginController 
+}
